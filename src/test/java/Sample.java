294d2f0be5afc426c446709e51f22c60173ab01b
gowthami <gowthamisundharaj@gmail.com>
2022-08-26 21:17:42 -0500

    added testcase

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,9 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Sample {
+    WebDriver driver;
+
+
     @Test
-    public void test()
-    {
+    public void starting_point() {
+
+        WebDriverManager.chromedriver().setup();
         System.out.println("welcome");
+        driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+        driver.close();
     }
 }
